4e321d0ce07ff52eb644974485fe051604dfda6c
DIDIQi <67574085+DIDIQi@users.noreply.github.com>
2020-08-07 23:02:57 +0800

    Update fishes.js

--- js/fishes.js
+++ js/fishes.js
@@ -7,7 +7,7 @@ function fish() {
         top: 0,
         right: 0,
         left: 0,
-        bottom: -5,
+        bottom: 0,
       }),
       $("footer").append(
         '<div class="container" id="jsi-flying-fish-container"></div>'
